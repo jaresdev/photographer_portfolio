29582a9c38a44c42f944f61fc16a8b62f9d914be
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-13 16:53:19 +0200

    Extract recent work data

---
+++ src/lib/data.ts
@@ -0,0 +1,29 @@
+export interface RecentWork {
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+}
+
+export const recentWorks: RecentWork[] = [
+  {
+    title: "Sydney Opera House",
+    subtitle: "Sydney",
+    description: `
+      Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+    `,
+    image: "/Sydney.jpg"
+  },
+  {
+    title: "Sydney Opera House",
+    subtitle: "Sydney",
+    description: `
+      Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+    `,
+    image: "/Sydney.jpg"
+  }
+]
