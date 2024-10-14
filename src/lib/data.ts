e2bea11a1cac3cf5e30e87a6f38aaebe307bde15
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-14 14:54:12 +0200

    Add new album example

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -25,5 +25,15 @@ export const albums: Album[] = [
           Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
     `,
     image: "/Sydney.jpg"
+  },
+  {
+    title: "Sydney Opera House",
+    subtitle: "Sydney",
+    description: `
+      Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+          Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
+    `,
+    image: "/Sydney.jpg"
   }
 ]
