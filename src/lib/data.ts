50349cdeea896cc592bc122c92d6de57a2b92f63
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-14 15:31:15 +0200

    Create recentWorks data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,6 +5,13 @@ export interface Album {
   image: string;
 }
 
+export   interface RecentWork {
+  title: string,
+  subtitle?: string,
+  content: string,
+  image: string
+}
+
 export const albums: Album[] = [
   {
     title: "Sydney Opera House",
@@ -35,5 +42,28 @@ export const albums: Album[] = [
           Lorem ipsum dolor amet, consectetur adipiscing elit. Sed est, tellus faucibus mi amet, metus. A tempus at odio quisque nec non eget varius commodo aliquet tincidunt.
     `,
     image: "/Sydney.jpg"
+  }
+]
+
+export const recentWorks: RecentWork[] = [
+  {
+    image: "/banner_tmp.png",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat."
+  },
+  {
+    image: "/banner_tmp.png",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat."
+  },
+  {
+    image: "/banner_tmp.png",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat."
+  },
+  {
+    image: "/banner_tmp.png",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat."
   }
 ]
