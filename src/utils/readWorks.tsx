f25ae09f8c938d4112c6ba9e447183c71efcddcc
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-15 13:29:00 +0200

    Change function name

--- src/utils/readWorks.tsx
+++ src/utils/readWorks.tsx
@@ -1,17 +1,17 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 
-export async function readDirectory(directory: string) {
-  const dirPath = path.join(process.cwd(), directory);
+export async function getImagesFromDirectory(directory: string) {
+  const dirPath = path.join(process.cwd(), directory)
   try {
-    const files = await fs.readdir(dirPath);
+    const files = await fs.readdir(dirPath)
     const imageFiles = files.filter(file => {
-      const ext = path.extname(file).toLowerCase();  // Obtiene la extensión del archivo
-      return ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg'].includes(ext);  // Extensiones de imagen
+      const ext = path.extname(file).toLowerCase()  // Obtiene la extensión del archivo
+      return ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg'].includes(ext)  // Extensiones de imagen
     });
-    return imageFiles;
+    return imageFiles
   } catch (error) {
-    console.error('Error reading directory:', error);
-    return [];
+    console.error('Error reading directory:', error)
+    return []
   }
 }
