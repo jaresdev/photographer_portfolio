622c55cc36f307b4f2cc365f299ba0c487a5fa19
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-15 13:41:56 +0200

    Change property name

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,12 +7,12 @@ export interface Album {
   directory: string;
 }
 
-export interface RecentWork {
+export interface Work {
   id: string,
   title: string,
   subtitle?: string,
   description: string,
-  image: string,
+  thumbnail: string,
   directory: string
 }
 
@@ -55,31 +55,31 @@ export const favAlbums: Album[] = [
   }
 ]
 
-export const recentWorks: RecentWork[] = [
+export const recentWorks: Work[] = [
   {
     id: "01928eeb-583f-723f-845c-c083e81449fc",
-    image: "works/work1/1.jpg",
+    thumbnail: "works/work1/1.jpg",
     title: "Lorem ipsum",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat.",
     directory: "work1"
   },
   {
     id: "01928eeb-f267-79ef-b8a1-ff748cc7cd1d",
-    image: "works/work2/2.jpg",
+    thumbnail: "works/work2/2.jpg",
     title: "Lorem ipsum",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat.",
     directory: "work2"
   },
   {
     id: "01928eec-1782-7f3a-8b6f-ae60b7523080",
-    image: "works/work3/3.jpg",
+    thumbnail: "works/work3/3.jpg",
     title: "Lorem ipsum",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat.",
     directory: "work3"
   },
   {
     id: "01928eec-30be-76d0-a4ec-84a2b7d37f41",
-    image: "works/work4/4.jpg",
+    thumbnail: "works/work4/4.jpg",
     title: "Lorem ipsum",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mattis nisi. <strong>Phasellus et leo ullamcorper, maximus arcu in</strong>, tincidunt ipsum. Aliquam interdum vehicula lectus eget congue. Donec porta justo eget libero ultrices pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae diam sem. Duis quis leo eget tellus ornare rutrum sit amet vitae arcu. Duis vehicula tortor sed mi egestas volutpat.",
     directory: "work4"
