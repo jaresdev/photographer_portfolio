f51cad262981bcf5d11a32e150fa71265429a0eb
Jose Arroyo <josearroyoescudero@gmail.com>
2024-10-15 14:34:54 +0200

    Sort by date

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 
-import { works  } from '@/lib/data';
+import { Work, works  } from '@/lib/data';
 
 export async function getImagesFromDirectory (directory: string) {
   const dirPath = path.join(process.cwd(), directory)
@@ -20,7 +20,9 @@ export async function getImagesFromDirectory (directory: string) {
 
 export async function getRecentWorks () {
   try {
-    return works.slice(0,4)
+    return works
+    .sort((a: Work, b: Work) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0,4)
   } catch (error) {
     console.error('No works! ', error)
     return []
